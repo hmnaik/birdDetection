28173d6f72ea43a64d3973b88d40152552844f71
hmnaik <naik@in.tum.de>
2018-06-21 17:27:30 +0200

    Added new file to get annotation from loopy and tune them to match with input of detection, Also changed object detection testing script to work with both images and videos

---
+++ loopy_annotation_cleanup.py
@@ -0,0 +1,10 @@
+
+# The file is supposed to clean up the annotation provided by loopy
+
+path = '/media/hemal/37960676-e41c-4a31-ab6a-e01a2521f68b/hemal/BirdDataset/MPI_Dataset'
+
+video_dir = path + 'video'
+annotation_dir = path + 'Annotations'
+
+
+
