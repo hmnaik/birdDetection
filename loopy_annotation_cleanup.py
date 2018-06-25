977b8d07f6a7904c3678f58a76f16809ef269552
Hema Naik <hnaik@orn.mpg.de>
2018-06-25 15:28:58 +0200

    * Index problem while writing .csv, important to specify index = False to not have extra column. * Generating one mega file for data, just like CUB dataset to be used with object Detection API

--- loopy_annotation_cleanup.py
+++ loopy_annotation_cleanup.py
@@ -6,6 +6,8 @@ import glob
 import os
 import numpy as np
 import cv2 as cv
+from pandas.io.sas.sas7bdat import _column
+
 
 def copyContent(annotationData,revisedAnnotation,video):
 
@@ -21,9 +23,7 @@ def copyContent(annotationData,revisedAnnotation,video):
         os.mkdir(imageDir)
 
     cap = cv.VideoCapture(video)
-
     print(cap.isOpened())
-    counter = 0
 
     # We loop through all the columns and pick the relevant frames, to store the csv file relevant to that
     for i in range(revisedAnnotation.shape[0]):
@@ -42,8 +42,11 @@ def copyContent(annotationData,revisedAnnotation,video):
             revisedAnnotation.loc[i, "Channel"] = c
             # Saving hierarchial names --> videoName/imageName --> Path X:\xx\xx\videoName\images_frameNo
             revisedAnnotation.loc[i, "img_name"] = videoName + '/' +imageName
-            cv.imwrite(imagePath, frame)
-            cv.imshow("TestImage", frame)
+
+            # Saving the images for the training
+            if SAVE_JPEG_IMG: # Check to avoid repeated generation of files
+                cv.imwrite(imagePath, frame)
+                cv.imshow("TestImage", frame)
 
     # Release the video footage
     cap.release()
@@ -57,45 +60,65 @@ def createDataBase(annotation,video):
     annotationData = pd.read_csv(annotationFile, sep=",", header=None)
     annotationData.columns = ["index","annotation_id","class","date","frame_count","frame_number","frame_timestamp","name","oid","type","video_id","x","x0","x1","x2","x3","xform","y","y0","y1","y2","y3"]
     annotationData = annotationData.loc[1:,:]
-    annotationData.reset_index(inplace = True)
+    annotationData.reset_index(inplace = True) # Reset of index is important, otherwise the first row element starts from 1, row 0 is deleted for good unless reset.
 
     revisedAnnotationColumnFormat = ["img_id","x","y","width","height","img_name","img_width","img_height","Channel","train_status"]
-    revisedAnnotation = pd.DataFrame( np.zeros((annotationData.shape[0],10)),
-                                      columns = revisedAnnotationColumnFormat )
-
+    #revisedAnnotation = pd.DataFrame( np.zeros((annotationData.shape[0],len(revisedAnnotationColumnFormat))),
+                                      # columns = revisedAnnotationColumnFormat )
+    revisedAnnotation = pd.DataFrame(columns = revisedAnnotationColumnFormat )
+    print(revisedAnnotation)
     copyContent(annotationData,revisedAnnotation,video)
     return revisedAnnotation
 
 
 ####
-DATASET_PATH = 'D:/BirdTrackingProject/MPI_Dataset'
-video_dir =  DATASET_PATH + '/videoDataset/'
-annotation_dir = DATASET_PATH + '/Annotations/'
+SAVE_JPEG_IMG = False
+DATA_FORMAT = ["img_id","x","y","width","height","img_name","img_width","img_height","Channel","train_status"]
 
-videoFiles = glob.glob(video_dir + '*.mp4')
-annotationFiles = glob.glob(annotation_dir + '*.csv')
+# The function is used to initiate the function when it is called directly from console
+def main(DATASET_PATH):
 
-for files in videoFiles:
-    print(files)
+    # Derive video and annotation path from the given directory path
+    video_dir =  DATASET_PATH + '/videoDataset/'
+    annotation_dir = DATASET_PATH + '/Annotations/'
+    combinedDataFileName = "loopBio_dataset.csv"
 
-if len(videoFiles) != len(annotationFiles):
-    raise ('Error!! Video file and Annotations files do not match')
+    # Grab name of the files given in the header folder
+    videoFiles = glob.glob(video_dir + '*.mp4')
+    annotationFiles = glob.glob(annotation_dir + '*.csv')
+
+    for files in videoFiles:
+        print(files)
+
+    if len(videoFiles) != len(annotationFiles):
+        raise ('Error!! Video file and Annotations files do not match')
+
+    # Meta file for storing data
+    combineData = pd.DataFrame()
+
+    for annotation in annotationFiles:
+        annotationFileName = os.path.basename(annotation)
+        annotationFileNameWoExt = os.path.splitext(annotationFileName)[0]
+        for video in videoFiles:
+            videoFileName = os.path.basename(video)
+            videoFileNameWoExt = os.path.splitext(videoFileName)[0]
+            if videoFileNameWoExt in annotationFileNameWoExt:
+                combinedAnnotation = createDataBase(annotation,video) # Send both file names
+                annotationFileName = DATASET_PATH + '/' + videoFileNameWoExt + '.csv'
+                combinedAnnotation.to_csv( annotationFileName, index = False )
+                combineData = combineData.append(combinedAnnotation)
 
 
+    # Change the first column to have proper ID assigned to images
+    combineData.iloc[:,0] = list(range(combineData.shape[0]))
+    combineData.to_csv(combinedDataFileName, index = False)
 
-for annotation in annotationFiles:
-    annotationFileName = os.path.basename(annotation)
-    annotationFileNameWoExt = os.path.splitext(annotationFileName)[0]
-    for video in videoFiles:
-        videoFileName = os.path.basename(video)
-        videoFileNameWoExt = os.path.splitext(videoFileName)[0]
-        if videoFileNameWoExt in annotationFileNameWoExt:
-            combinedAnnotation = createDataBase(annotation,video) # Send both file names
-            annotationFileName = DATASET_PATH + '/' + videoFileNameWoExt + '.csv'
-            combinedAnnotation.to_csv( annotationFileName )
-
-
-# First read the folder for the video information
-
-
+# The function is called only if the file is called on its own, this enables using files independently
+if __name__ == '__main__':
+    print('Called from independent console')
+    DATASET_PATH = 'D:/BirdTrackingProject/MPI_Dataset'
+    main(DATASET_PATH)
+    print('Conversion Process Ends!!')
+else:
+    print('Called by third party!!')
 # Second read the folder for the csv information
